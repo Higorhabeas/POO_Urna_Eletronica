9dcef46733616fa523da795134919b0dbe343f7a
Higor <higoras.silva0@gmail.com>
2024-06-10 19:07:46 -0300

    Primeiro commit com urna lendo candidatos mas não voltando para o segundo voto

---
+++ Form1.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace POO_Urna_Eletronica
+{
+    partial class frmUrna
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            btnBranco = new Button();
+            btnCorrige = new Button();
+            btnConfirma = new Button();
+            panelFim = new Panel();
+            panelPrincipal = new Panel();
+            picFotoCandidato = new PictureBox();
+            lblPartidoCandidato = new Label();
+            lblNomeCandidato = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            txtUnidade = new TextBox();
+            txtDecimal = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            lFim = new Label();
+            panelFim.SuspendLayout();
+            panelPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picFotoCandidato).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(64, 64, 64);
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.Window;
+            button1.Location = new Point(489, 238);
+            button1.Name = "button1";
+            button1.Size = new Size(38, 27);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(64, 64, 64);
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.ForeColor = SystemColors.Window;
+            button2.Location = new Point(535, 238);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 27);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(64, 64, 64);
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.ForeColor = SystemColors.Window;
+            button3.Location = new Point(588, 238);
+            button3.Name = "button3";
+            button3.Size = new Size(38, 27);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(64, 64, 64);
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.ForeColor = SystemColors.Window;
+            button4.Location = new Point(489, 281);
+            button4.Name = "button4";
+            button4.Size = new Size(38, 27);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(64, 64, 64);
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.ForeColor = SystemColors.Window;
+            button5.Location = new Point(535, 281);
+            button5.Name = "button5";
+            button5.Size = new Size(38, 27);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(64, 64, 64);
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.ForeColor = SystemColors.Window;
+            button6.Location = new Point(588, 281);
+            button6.Name = "button6";
+            button6.Size = new Size(38, 27);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(64, 64, 64);
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.ForeColor = SystemColors.Window;
+            button7.Location = new Point(489, 325);
+            button7.Name = "button7";
+            button7.Size = new Size(38, 27);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.TextAlign = ContentAlignment.MiddleLeft;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(64, 64, 64);
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.ForeColor = SystemColors.Window;
+            button8.Location = new Point(535, 325);
+            button8.Name = "button8";
+            button8.Size = new Size(38, 27);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.TextAlign = ContentAlignment.MiddleLeft;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(64, 64, 64);
+            button9.FlatStyle = FlatStyle.Popup;
+            button9.ForeColor = SystemColors.Window;
+            button9.Location = new Point(588, 325);
+            button9.Name = "button9";
+            button9.Size = new Size(38, 27);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.TextAlign = ContentAlignment.MiddleLeft;
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(64, 64, 64);
+            button10.FlatStyle = FlatStyle.Popup;
+            button10.ForeColor = SystemColors.Window;
+            button10.Location = new Point(535, 362);
+            button10.Name = "button10";
+            button10.Size = new Size(38, 27);
+            button10.TabIndex = 10;
+            button10.Text = "0";
+            button10.TextAlign = ContentAlignment.MiddleLeft;
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // btnBranco
+            // 
+            btnBranco.FlatStyle = FlatStyle.Flat;
+            btnBranco.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBranco.Location = new Point(467, 402);
+            btnBranco.Name = "btnBranco";
+            btnBranco.Size = new Size(52, 28);
+            btnBranco.TabIndex = 11;
+            btnBranco.Text = "BRANCO";
+            btnBranco.TextAlign = ContentAlignment.TopCenter;
+            btnBranco.UseVisualStyleBackColor = true;
+            btnBranco.Click += button11_Click;
+            // 
+            // btnCorrige
+            // 
+            btnCorrige.BackColor = Color.FromArgb(255, 128, 0);
+            btnCorrige.Cursor = Cursors.Hand;
+            btnCorrige.FlatStyle = FlatStyle.Flat;
+            btnCorrige.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCorrige.Location = new Point(531, 400);
+            btnCorrige.Name = "btnCorrige";
+            btnCorrige.Size = new Size(55, 30);
+            btnCorrige.TabIndex = 12;
+            btnCorrige.Text = "CORRIGE";
+            btnCorrige.TextAlign = ContentAlignment.TopCenter;
+            btnCorrige.UseVisualStyleBackColor = false;
+            btnCorrige.Click += btnCorrige_Click;
+            // 
+            // btnConfirma
+            // 
+            btnConfirma.BackColor = Color.FromArgb(0, 192, 0);
+            btnConfirma.Cursor = Cursors.Hand;
+            btnConfirma.FlatStyle = FlatStyle.Flat;
+            btnConfirma.Font = new Font("Segoe UI", 6F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnConfirma.Location = new Point(594, 393);
+            btnConfirma.Name = "btnConfirma";
+            btnConfirma.Size = new Size(56, 37);
+            btnConfirma.TabIndex = 13;
+            btnConfirma.Text = "CONFIRMA";
+            btnConfirma.TextAlign = ContentAlignment.TopCenter;
+            btnConfirma.UseVisualStyleBackColor = false;
+            btnConfirma.Click += btnConfirma_Click;
+            // 
+            // panelFim
+            // 
+            panelFim.Controls.Add(panelPrincipal);
+            panelFim.Controls.Add(lFim);
+            panelFim.Location = new Point(31, 188);
+            panelFim.Name = "panelFim";
+            panelFim.Size = new Size(394, 234);
+            panelFim.TabIndex = 14;
+            // 
+            // panelPrincipal
+            // 
+            panelPrincipal.Controls.Add(picFotoCandidato);
+            panelPrincipal.Controls.Add(lblPartidoCandidato);
+            panelPrincipal.Controls.Add(lblNomeCandidato);
+            panelPrincipal.Controls.Add(label4);
+            panelPrincipal.Controls.Add(label3);
+            panelPrincipal.Controls.Add(txtUnidade);
+            panelPrincipal.Controls.Add(txtDecimal);
+            panelPrincipal.Controls.Add(label2);
+            panelPrincipal.Controls.Add(label1);
+            panelPrincipal.Location = new Point(3, 0);
+            panelPrincipal.Name = "panelPrincipal";
+            panelPrincipal.Size = new Size(392, 234);
+            panelPrincipal.TabIndex = 15;
+            //panelPrincipal.Paint += panelPrincipal_Paint;
+            // 
+            // picFotoCandidato
+            // 
+            picFotoCandidato.Location = new Point(244, 41);
+            picFotoCandidato.Name = "picFotoCandidato";
+            picFotoCandidato.Size = new Size(132, 139);
+            picFotoCandidato.TabIndex = 8;
+            picFotoCandidato.TabStop = false;
+            // 
+            // lblPartidoCandidato
+            // 
+            lblPartidoCandidato.AutoSize = true;
+            lblPartidoCandidato.Location = new Point(70, 178);
+            lblPartidoCandidato.Name = "lblPartidoCandidato";
+            lblPartidoCandidato.Size = new Size(16, 15);
+            lblPartidoCandidato.TabIndex = 7;
+            lblPartidoCandidato.Text = "...";
+            // 
+            // lblNomeCandidato
+            // 
+            lblNomeCandidato.AutoSize = true;
+            lblNomeCandidato.Location = new Point(70, 157);
+            lblNomeCandidato.Name = "lblNomeCandidato";
+            lblNomeCandidato.Size = new Size(16, 15);
+            lblNomeCandidato.TabIndex = 6;
+            lblNomeCandidato.Text = "...";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(9, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Partido:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 156);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Nome:";
+            // 
+            // txtUnidade
+            // 
+            txtUnidade.BackColor = SystemColors.Control;
+            txtUnidade.BorderStyle = BorderStyle.FixedSingle;
+            txtUnidade.Location = new Point(105, 77);
+            txtUnidade.Name = "txtUnidade";
+            txtUnidade.Size = new Size(34, 23);
+            txtUnidade.TabIndex = 3;
+            // 
+            // txtDecimal
+            // 
+            txtDecimal.BackColor = SystemColors.Control;
+            txtDecimal.BorderStyle = BorderStyle.FixedSingle;
+            txtDecimal.Location = new Point(65, 77);
+            txtDecimal.Name = "txtDecimal";
+            txtDecimal.Size = new Size(34, 23);
+            txtDecimal.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Número:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(65, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 25);
+            label1.TabIndex = 0;
+            label1.Text = "PRESIDENTE";
+            // 
+            // lFim
+            // 
+            lFim.AutoSize = true;
+            lFim.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lFim.Location = new Point(167, 85);
+            lFim.Name = "lFim";
+            lFim.Size = new Size(77, 47);
+            lFim.TabIndex = 0;
+            lFim.Text = "FIM";
+            // 
+            // frmUrna
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Urna_2;
+            ClientSize = new Size(696, 450);
+            Controls.Add(panelFim);
+            Controls.Add(btnConfirma);
+            Controls.Add(btnCorrige);
+            Controls.Add(btnBranco);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmUrna";
+            Text = "Urna";
+            panelFim.ResumeLayout(false);
+            panelFim.PerformLayout();
+            panelPrincipal.ResumeLayout(false);
+            panelPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picFotoCandidato).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button btnBranco;
+        private Button btnCorrige;
+        private Button btnConfirma;
+        private Panel panelFim;
+        private Label lFim;
+        private Panel panelPrincipal;
+        private Label label1;
+        private TextBox txtDecimal;
+        private Label label2;
+        private TextBox txtUnidade;
+        private Label lblNomeCandidato;
+        private Label label4;
+        private Label label3;
+        private PictureBox picFotoCandidato;
+        private Label lblPartidoCandidato;
+        //private System.Windows.Forms.Timer relogio;
+        
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+using System.Media;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace POO_Urna_Eletronica
+{
+    public partial class frmUrna : Form
+    {
+        private Dictionary<string, Candidato> _dicCandidato;
+        private System.Windows.Forms.Timer relogio; // Declarando o Timer
+
+
+        public frmUrna()
+        {
+            InitializeComponent();
+
+            _dicCandidato = new Dictionary<string, Candidato>();
+            _dicCandidato.Add("51", new Candidato() { Id = 51, Nome = "Darth Vader", Partido = "Império", Foto = Properties.Resources.DarthVader });
+            _dicCandidato.Add("52", new Candidato() { Id = 52, Nome = "Chapolin", Partido = "Quase Nada", Foto = Properties.Resources.Chapolin });
+            _dicCandidato.Add("91", new Candidato() { Id = 91, Nome = "Goku", Partido = "Saijin", Foto = Properties.Resources.Goku });
+            _dicCandidato.Add("92", new Candidato() { Id = 92, Nome = "Homem de Ferro", Partido = "Vingadores", Foto = Properties.Resources.HomemDeFerro });
+            _dicCandidato.Add("99", new Candidato() { Id = 99, Nome = "Batman", Partido = "Liga da Justiça", Foto = Properties.Resources.Batman });
+
+            relogio = new System.Windows.Forms.Timer();// Inicializando o Timer
+        }
+
+
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            panelFim.Visible = true;
+            Limpar();
+
+            SoundPlayer s = new SoundPlayer(Properties.Resources.urna);
+            s.Play();
+
+            relogio.Tick -= AcaoFinal;
+
+            relogio.Tick += new EventHandler(AcaoFinal);
+            relogio.Interval = 300;
+            relogio.Enabled = true;
+            relogio.Start();
+        }
+
+        private void AcaoFinal(Object? myObject, EventArgs myEventArgs)
+        {
+            relogio.Stop();
+            relogio.Enabled = false;
+            panelFim.Visible = false;
+            panelPrincipal.Visible = true;
+        }
+
+        private void RegistrarDigito(string digito)
+        {
+            if (string.IsNullOrEmpty(txtDecimal.Text))
+                txtDecimal.Text = digito;
+            else
+            {
+                txtUnidade.Text = digito;
+                PreencherCandidato(txtDecimal.Text, txtUnidade.Text);
+            }
+
+            SoundPlayer s = new SoundPlayer(Properties.Resources.clique);
+            s.Play();
+        }
+
+        private void PreencherCandidato(string d1, string d2)
+        {
+            if (_dicCandidato.ContainsKey(d1 + d2))
+            {
+                lblNomeCandidato.Text = _dicCandidato[d1 + d2].Nome;
+                lblPartidoCandidato.Text = _dicCandidato[d1 + d2].Partido;
+                picFotoCandidato.Image = _dicCandidato[d1 + d2].Foto;
+            }
+            else
+            {
+                MessageBox.Show("Candidato não encontrado!");
+            }
+        }
+
+        private void Limpar()
+        {
+            txtDecimal.Text = "";
+            txtUnidade.Text = "";
+            lblNomeCandidato.Text = String.Empty;
+            lblPartidoCandidato.Text = String.Empty;
+            picFotoCandidato.Image = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("9");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            RegistrarDigito("0");
+        }
+
+        private void btnCorrige_Click(object sender, EventArgs e)
+        {
+            Limpar();
+            relogio.Stop();
+            relogio.Enabled = false;
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDecimal.Text))
+            {
+                MessageBox.Show("Favor informar o candidato.");
+                return;
+            }
+
+            //panelFim.Visible = true;
+            //Limpar();
+            SoundPlayer s = new SoundPlayer(Properties.Resources.urna);
+            s.Play();
+
+            // Remova o EventHandler existente para evitar múltiplas associações
+            relogio.Tick -= AcaoFinal;
+
+            relogio.Tick += new EventHandler(AcaoFinal);
+            relogio.Interval = 300;
+            relogio.Enabled = true;
+            relogio.Start();
+            //InitializeComponent();
+        }
+
+       
+    }
+    public class Candidato
+    {
+        public int Id { get; set; }
+        public string ?Nome { get; set; }
+        public string ?Partido { get; set; }
+        public Image ?Foto { get; set; }
+    }
+}
